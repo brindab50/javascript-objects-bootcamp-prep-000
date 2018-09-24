4d57ad45c85eca5cc43b816dbf2c6ada8d8a335a
brindab50 behal <brindab50@gmail.com>
2018-09-24 18:08:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artist: "song title"
 };
 function updatePlaylist(playlist, artist, songTitle) 
-{ playlist[artist] 
+{ playlist[artist] = songTitle
   return playlist
 }
 function removeFromPlaylist(playlist, artist) {
